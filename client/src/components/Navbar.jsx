2b59415feaf19ae2f7c65696fa622eeac10fbf64
BenJensen2 <btjensen@mtu.edu>
2020-08-28 10:53:51 -0700

    Basic Collapsible NavBar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,45 @@
-import React from 'react';
-import Logo from '../images/logo.jpg'
+import React, { useState } from 'react';
+// import Logo from '../images/logo.jpg'
 import LinkedIn_Logo from '../images/linkedin.png'
 import Github_Logo from '../images/github3.png'
 import Email_Logo from '../images/email2.png'
-import '../styles/navbar.css'
+import '../styles/nav.css'
 
 
 const Navbar = (props) => {
+
+  const [navLinkClass, setNavLinkClass] = useState('nav_link')
+
+  const toggleHandler = (e) => {
+    console.log("Toggle Clicked")
+    if (navLinkClass == 'nav_link') {
+      setNavLinkClass('nav_link_show')
+    }
+    else { setNavLinkClass('nav_link') }
+  }
+
   return (
-    <div className="navbar">
-      <div className="navleft">
+    <nav className="navbar">
+      <div className="nav_items nav_left">
         <div className="nav_link">
           <a href="/">
-            <img className="name_logo" src={Logo} alt="Logo" />
+            {/* <img className="name_logo" src={Logo} alt="Logo" /> */}
           </a>
         </div>
-        <div className="nav_link">
+
+        <a id="nav_toggle" href="#" onClick={toggleHandler}>&#9776;</a>
+
+        <div className={navLinkClass}>
           <a id="projects_Link" href="/projects">Projects</a>
         </div>
-        <div className="nav_link">
+        <div className={navLinkClass}>
           <a href="/management">Management</a>
         </div>
-        <div className="nav_link">
+        <div className={navLinkClass}>
           <a href="/resources">Resources</a>
         </div>
       </div>
-      <div className="contact">
+      <div className="nav_items nav_right contact">
         <a href="https://www.linkedin.com/in/ben-jensen-68b62940/">
           <img className="contact_logo" src={LinkedIn_Logo} alt="Logo" />
         </a>
@@ -36,7 +50,7 @@ const Navbar = (props) => {
           <img className="contact_logo" src={Email_Logo} alt="" />
         </a>
       </div>
-    </div>
+    </nav>
   )
 }
 
